414c8e4de821fd3dc4f7c1889f1aa810c68375a6
SHUBHAM THAKUR <shubhamwert@gmail.com>
2019-08-29 00:05:04 +0530

    descipt frag added\n

--- app/src/main/java/com/stechapps/fakepost/activity_twitter_fake.java
+++ app/src/main/java/com/stechapps/fakepost/activity_twitter_fake.java
@@ -3,6 +3,8 @@ package com.stechapps.fakepost;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 public class activity_twitter_fake extends AppCompatActivity {
 
@@ -14,5 +16,13 @@ public class activity_twitter_fake extends AppCompatActivity {
 
 
 
+    }
+
+    public void create_tweet(View view) {
+        FrameLayout flm=findViewById(R.id.fl_frag_container);
+        TweetDescip tweetDescip=new TweetDescip(this);
+        getSupportFragmentManager().beginTransaction().add(R.id.fl_frag_container,tweetDescip).commit();
+        flm.setVisibility(View.VISIBLE);
+        findViewById(R.id.bt_create_twitter).setVisibility(View.GONE);
     }
 }
